2b28ad9023b71a7a1f962e0ab0b833e315658b85
Halemo GPA <haleemborham3@gmail.com>
2022-04-19 06:35:06 +0200

    last video experiment

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-var myname = "AbdelHaleem";
-console.log(`my name ${myname}`);
+let theTitle = "Elzero",
+  theDescription = "Elzero Web School",
+  theDate = "25/10";
+let markup = `<div>
+    <h3>Hello ${theTitle}</h3>
+    <p>${theDescription}</p>
+    <span>${theDate}</span>
+</div>`;
+document.write(markup.repeat(4));
