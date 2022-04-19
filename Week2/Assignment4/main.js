c612f2e28ba621c5448a6b703e0dc3434873d57f
Halemo GPA <haleemborham3@gmail.com>
2022-04-19 17:54:51 +0200

    adjustment

--- Week2/Assignment4/main.js
+++ Week2/Assignment4/main.js
@@ -1,8 +1,6 @@
 let a = 21;
 let b = 20;
-let d = a;
-let dd = a;
-let e = b;
-let ee = b;
+let dd = (d = a);
+let ee = (e = b);
 let c = d + "" + e;
 console.log(`_${d}_${(c + "_").repeat(++dd % --ee)} ${++e}_`);
